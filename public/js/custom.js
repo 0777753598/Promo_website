46047692f3aee61cc35d8bad7ee789cad749a6e5
Darshana <dashana.soa@gmail.com>
2017-07-31 23:17:26 +0530

    done some improvements

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,4 +1,8 @@
 // if we have three layers to be parallax we need three speeds to move them seperately
+	var chat = document.getElementById('innerWrapper');
+	console.log(chat);
+
+
 	var speed_1 = 0.5; 
 	var speed_2 = 0.3;
 	var speed_3 = 0.1;
@@ -22,4 +26,63 @@
 		val = "50%"+(pageYOffset*speed_3)+"px";
 		background.style.backgroundPosition = val;
 
+		console.log(val);
+
 	}
+
+// $(document).ready(function(){
+//     	$("#main_heading").effect("bounce",{times:3},2000);
+//     	$("#main_heading").mouseover(function(){
+//     		 $("#main_heading").effect("bounce",{times:2,distance:10,direction:"left"},1000);
+//     	});
+//     })    ;
+    // Closes the sidebar menu
+    $("#menu-close").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+    // Opens the sidebar menu
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+    // Scrolls to the selected menu item on the page
+    $(function() {
+        $('a[href*=#]:not([href=#],[data-toggle],[data-target],[data-slide])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name='+this.hash.slice(1)+']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+    //#to-top button appears after scrolling
+    var fixed = false;
+    $(document).scroll(function() {
+        if ($(this).scrollTop() > 250) {
+            if (!fixed) {
+                fixed = true;
+                // $('#to-top').css({position:'fixed', display:'block'});
+                $('#to-top').show("slow", function() {
+                    $('#to-top').css({
+                        position: 'fixed',
+                        display: 'block'
+                    });
+                });
+            }
+        } else {
+            if (fixed) {
+                fixed = false;
+                $('#to-top').hide("slow", function() {
+                    $('#to-top').css({
+                        display: 'none'
+                    });
+                });
+            }
+        }
+    });
